30d626a26f21803b36564b8b531bb0de6a8558d2
Amethnb2218 <amethnb2218@users.noreply.github.com>
2026-07-23 04:36:35 +0000

    update CloudFront timeout to 180s (max allowed)

--- deploy-fargate/fix-cloudfront-timeout.sh
+++ deploy-fargate/fix-cloudfront-timeout.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# Fix CloudFront origin timeout: 30s -> 120s
-
+# Fix CloudFront origin timeout to max (180s)
 DIST_ID="EYEAUA7F52S9D"
 
 echo "Getting current config..."
-aws cloudfront get-distribution-config --id $DIST_ID --output json > /tmp/cf.json
 ETAG=$(aws cloudfront get-distribution-config --id $DIST_ID --query "ETag" --output text)
+aws cloudfront get-distribution-config --id $DIST_ID --output json > /tmp/cf.json
 
-echo "Updating timeout to 120s..."
+echo "Setting timeout to 180s (CloudFront max)..."
 python3 -c "
 import json
 with open('/tmp/cf.json') as f:
     data = json.load(f)
 config = data['DistributionConfig']
 for origin in config['Origins']['Items']:
-    origin['CustomOriginConfig']['OriginReadTimeout'] = 120
+    origin['CustomOriginConfig']['OriginReadTimeout'] = 180
 with open('/tmp/cf-update.json', 'w') as f:
     json.dump(config, f, indent=2)
 "
@@ -22,4 +21,4 @@ with open('/tmp/cf-update.json', 'w') as f:
 aws cloudfront update-distribution --id $DIST_ID --if-match $ETAG --distribution-config file:///tmp/cf-update.json --query "Distribution.Status" --output text
 
 echo ""
-echo "Done! Timeout set to 120s. Wait ~2 min for propagation."
+echo "Done! Timeout = 180s. Wait ~2 min."
